9958d389dec2fda3dcc61006634b3e60c46da919
tzuhsun <hsungrass@gmail.com>
2023-10-14 23:22:59 +0800

    remote-week3: Add comment

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 const connection = require('./db');
 
-//set up CORS
+// set up CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1'); // change to frontend url
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
