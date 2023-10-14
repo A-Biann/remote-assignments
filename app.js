cfe125539e3404ac805c6984fa1347d73971513d
tzuhsun <hsungrass@gmail.com>
2023-10-14 09:44:18 +0800

    remote-week3: Enable CORS

--- app.js
+++ app.js
@@ -11,58 +11,23 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 const connection = require('./db');
 
+//set up CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 // Use the user routes
 app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.send('This is root page');
 });
 
 app.get('/healthcheck', (req, res) => {
     res.send('This is health check page');
-});
-
-app.post('/try/users', (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-  
-      // validate the data
-        if (!validateUserData(name, email, password)) {
-            console.log('Error Message: Invalid data')
-            return res.status(400).json({ error: 'Error Message: Invalid data' });
-        }
-  
-        const insertQuery = `INSERT INTO user (name, email, password) VALUES (?, ?, ?)`;
-        connection.query(insertQuery, [name, email, password], (err, results) => {
-            if (err) {
-                if (err.code === 'ER_DUP_ENTRY') {
-                console.log('Error Message: Email already exists');
-                return res.status(409).json({ error: 'Error Message: Email already exists' });
-                }
-                console.log(err);
-                return res.status(500).json({ error: 'Error Message: Server error when inserting' });
-            }
-  
-            const userId = results.insertId;
-            const userObject = {
-                id: userId,
-                name,
-                email
-            };
-  
-            const response = {
-                data: {
-                    user: userObject, 
-                    'request-date': new Date().toUTCString() 
-                }   
-            };
-    
-            res.status(200).json(response);
-        });
-    } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).json({ error: 'Error Message: Server error' });
-    }
 });
 
 app.listen(port, () => {
